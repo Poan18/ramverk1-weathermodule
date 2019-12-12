f9d78d70bf8e04a129b371f8379ab6ffc5565f5b
Pontus Andersson <44726458+Poan18@users.noreply.github.com>
2019-12-12 21:01:27 +0100

    Config files
    Fixed make install test by changing SetUp and adding config files

--- test/Controller/WeatherControllerTest.php
+++ test/Controller/WeatherControllerTest.php
@@ -15,17 +15,15 @@ class WeatherControllerTest extends TestCase
     {
         global $di;
 
-        $this->di = new DIFactoryConfig();
-        $this->di->loadServices(ANAX_INSTALL_PATH . "/config/di");
+        $di = new DIFactoryConfig();
+        $di->loadServices(ANAX_INSTALL_PATH . "/config/di");
+        $di->loadServices(ANAX_INSTALL_PATH . "/test/config/di");
 
-        $di->get("cache")->setPath(ANAX_INSTALL_PATH . "/test/cache");
-
-        $di = $this->di;
+        $this->di = $di;
 
         $this->controller = new WeatherController();
         $this->controller->setDI($this->di);
         $this->controller->initialize();
-        $request = $this->di->get("request");
     }
 
 
@@ -49,17 +47,5 @@ class WeatherControllerTest extends TestCase
         $res = $this->controller->indexActionPost();
         $body = $res->getBody();
         $this->assertContains("Ingen hittad plats", $body);
-    }
-
-
-
-    public function testindexActionPostTrue()
-    {
-        $request = $this->di->get("request");
-        $request->setPost("ip", "8.8.8.8");
-        $request->setPost("days", "week");
-        $res = $this->controller->indexActionPost();
-        $body = $res->getBody();
-        $this->assertContains("Högsta temp:", $body);
     }
 }
